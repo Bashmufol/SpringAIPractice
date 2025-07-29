92686441f20c49ba8708586a6332d731ce467e49
Bashir Muhammed <bashmufol@gmail.com>
2025-07-29 22:16:50 +0100

    updated

--- src/main/java/com/bash/springaipractice/controller/OpenAIController.java
+++ src/main/java/com/bash/springaipractice/controller/OpenAIController.java
@@ -1,10 +1,17 @@
 package com.bash.springaipractice.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/openai")
 public class OpenAIController {
 
+    @GetMapping("/{message}")
+    public ResponseEntity<String> getAnswer(@PathVariable String message){
+        return  ResponseEntity.ok().body("Hello World");
+    }
 }
